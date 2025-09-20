866bc66b9157b996684b42203ca7e4b095270773
gabrielms-castro <gabrielms.castro@gmail.com>
2025-09-20 20:32:04 -0300

    fix: fixed resetHandler() parameter

--- src/commands/reset_command.ts
+++ src/commands/reset_command.ts
@@ -1,6 +1,6 @@
 import { deleteAllUsers } from "src/lib/db/queries/users";
 
-export async function resetHandler(_: string[]) {
+export async function resetHandler(_: string) {
     const resetUsers = await deleteAllUsers();
     console.log(`All users deleted: ${JSON.stringify(resetUsers)}`);
 }
